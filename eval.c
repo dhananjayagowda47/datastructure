aa171888213b1f314a864ce5294bf2a8769c37af
dhananjayagowda47 <100307296+dhananjayagowda47@users.noreply.github.com>
2022-05-10 20:03:33 +0530

    Add files via upload

---
+++ eval.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<math.h>
+int stack[30];
+char postfix[30];
+int top=-1;
+ 
+void push(int symbol)
+{
+stack[++top]=symbol;
+}
+int pop()
+{
+int symbol;
+symbol=stack[top--];
+return symbol;
+}
+int evalpost()
+{
+char symbol;
+int i=0,op1,op2;
+while((symbol=postfix[i++])!='\0')
+{
+if(isalnum(symbol))
+push(symbol-'0');
+else
+{
+op2=pop();
+op1=pop();
+switch(symbol)
+{
+case '+':push(op1+op2);
+          break;
+case '-':push(op1-op2);
+          break;
+case '*':push(op1*op2);
+          break;
+case '/':if(op2!=0)
+           {
+         push(op1/op2);
+            break;    
+             }
+          else
+          {
+           printf("out loop");
+            return 0;
+                 }
+case '^': push(pow(op1,op2));
+           break;
+default :return 0;
+}
+}}
+return(pop());
+}
+void main()
+{ 
+int val;
+printf("enter postfix expression");
+scanf("%s",postfix);
+
+val=evalpost();
+printf("evaluated expression:%d\n",val);
+}
